58028fbca3f9a89dcfe94fbf7ca5220f77fafd24
MaCode133 <74118184+MaCode133@users.noreply.github.com>
2020-11-07 18:55:35 -0300

    Codigo para mover al personaje con el teclado.

--- keyboard_movement.gd
+++ keyboard_movement.gd
@@ -8,13 +8,13 @@ var direction = Vector2()
 func obtener_entradas():
 	direction = Vector2()
 	
-	if Input.is_action_pressed("derecha"): # Mover hacia la derecha
-		direction.x += 1
-	if Input.is_action_pressed("left"): # Mover hacia la izquierda
+	if Input.is_action_pressed("derecha"): # Derecha
+		direction.x += 1 
+	if Input.is_action_pressed("left"): # Izquierda
 		direction.x -= 1
-	if Input.is_action_pressed("arriba"): # Mover hacia arriba
+	if Input.is_action_pressed("arriba"): # Arriba
 		direction.y -= 1
-	if Input.is_action_pressed("abajo"): # Mover hacia abajo
+	if Input.is_action_pressed("abajo"): # Abajo
 		direction.y += 1
 	
 	direction = direction.normalized() * speed # Mover diagonalmente al personaje sin sumar los valores
